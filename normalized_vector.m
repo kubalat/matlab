13a32fd47ec23593634dc8fbbe8caa310824446a
kubalat <kubala.tomas@hotmail.com>
2018-11-13 13:46:08 +0100

    Add smth juicy

---
+++ normalized_vector.m
@@ -0,0 +1,5 @@
+function [outputArg1] = normalized_vector(v1)
+%sprav normalizovany vektor
+outputArg1 = v1/max(v1);
+end
+
---
+++ sucet.m
@@ -0,0 +1,6 @@
+function [vygrcne] = Eunika(inputArg1,inputArg2)
+
+outputArg1 = inputArg1+inputArg2;
+
+end
+
